d57265e856b1e28c62fad5dcf6917bccb88fa485
Serg957 <sh.s.e.57@gmail.com>
2024-07-12 20:15:08 +0300

    Отправляю задание модуль2.2

---
+++ modul2.2.py
@@ -0,0 +1,41 @@
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
+if first == second == third:
+    print(3)
+elif first == second and second != third:
+    print(2)
+elif first != second and second == third:
+    print(2)
+elif first == third and second != third:
+    print(2)
+elif first != second != third:
+    print(0)
+
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
+if first == second == third:
+    print(3)
+elif first == second and second != third:
+    print(2)
+elif first != second and second == third:
+    print(2)
+elif first == third and second != third:
+    print(2)
+elif first != second != third:
+    print(0)
+
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
+if first == second == third:
+    print(3)
+elif first == second and second != third:
+    print(2)
+elif first != second and second == third:
+    print(2)
+elif first == third and second != third:
+    print(2)
+elif first != second != third:
+    print(0)
