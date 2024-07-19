3e0314d59ed7a26de3a12a557e1c63e252065924
Serg957 <sh.s.e.57@gmail.com>
2024-07-19 14:12:35 +0300

    Отправляю задание модуль 5.1

---
+++ module_5_1.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        new_floor += 1
+
+        for i in range(1, new_floor):
+            if 1 <= new_floor <= self.number_of_floors:
+                print(i)
+            else:
+                print('Такого этажа нет')
+                return
+
+h1 = House('ЖК Эльбрус', 30)
+h1.go_to(5)
+h1.go_to(31)
