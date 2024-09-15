10889e4de6a14502b630cd50f35f63a198eacac3
Сергей <sh.s.e.57@gmail.com>
2024-09-15 21:37:17 +0300

    Отправляю задание модуль 8_3

---
+++ module_8_3.py
@@ -0,0 +1,65 @@
+class Car:
+    def __init__(self, model, vin, numbers):
+        self.model = model
+        if not self.__is_valid_vin(vin):
+            raise IncorrectVinNumber('Некорректный vin номер')
+        if not self.__is_valid_numbers(numbers):
+            raise IncorrectCarNumbers('Некорректные номера')
+        self.__vin = vin
+        self.__numbers = numbers
+
+    def __is_valid_vin(self, vin):
+        if isinstance(vin, int):
+            if 1000000 <= vin <= 9999999:
+                return True
+            else:
+                raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        else:
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+
+    def __is_valid_numbers(self, numbers):
+        if isinstance(numbers, str):
+            if len(numbers) == 6:
+                return True
+            else:
+                raise IncorrectCarNumbers('Неверная длина номера')
+        else:
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+
+
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+try:
+    first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{first.model} успешно создан')
+
+try:
+    second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{second.model} успешно создан')
+
+try:
+    third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{third.model} успешно создан')
