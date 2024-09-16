739b61e030b814c7d84799cd9b972e4a73f9dc02
Сергей <sh.s.e.57@gmail.com>
2024-09-16 22:26:12 +0300

    Отправляю задание модуль 9.2

---
+++ module_9_3.py
@@ -0,0 +1,10 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+
+first_result = (len(word[0]) - len(word[1]) for word in  zip(first, second) if len(word[0]) !=len(word[1]))
+
+second_result = (len(first[index]) == len(second[index]) for index in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
