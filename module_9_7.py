089f52bd127e6e489e0367882db447921130c200
Сергей <sh.s.e.57@gmail.com>
2024-09-20 20:23:29 +0300

    Отправляю задание модуль_9_7

--- module_9_7.py
+++ module_9_7.py
@@ -1,11 +1,12 @@
 def is_prime(func):
     def wrapper(*args):
         n = func(*args)
-        if n <= 1:
+        if n == 1:
             return n
-        for i in range(2, int(n ** 0.5) + 1):
+        for i in  range(2, int(n ** 0.5) + 1):
             if n % i == 0:
                 return f'Составное\n{n}'
+
             else:
                 return f'Простое\n{n}'
     return wrapper
@@ -16,3 +17,4 @@ def sum_three(*args):
 
 result = sum_three(2, 3, 6)
 print(result)
+
