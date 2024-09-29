62e0581ab6a48d26f68b88e0c67d58d111a5d52f
Сергей <sh.s.e.57@gmail.com>
2024-09-29 18:49:46 +0300

    Отправляю 6_0_0

--- module_11_2.py
+++ module_11_2.py
@@ -1,21 +1,18 @@
-import inspect
 
+import inspect
 def introspection_info(obj):
-    obj_type = type(obj).__name__
-    attributes = dir(obj)
+    obj_type = type(obj)
+    obj_attributes = dir(obj)
 
     def methods():
         return methods.__dir__()
 
     module = obj.__class__.__module__
 
-    dictionary = {'type': obj_type, 'attributes': attributes, 'methods': methods(), 'module': module}
+
+    dictionary = {'type': obj_type, 'attributes': obj_attributes, 'methods': methods(), 'module': module}
     return dictionary
 
 
-info = introspection_info(48)
-print(info)
-string_info = introspection_info('Строка')
-print(string_info)
-dict_info = introspection_info({'Ряд': 9})
-print(dict_info)
+number_info = introspection_info(42)
+print(number_info)
