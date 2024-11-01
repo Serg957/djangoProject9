34e21c70238ef5fe300ce7d87f2b5919039b54ff
Сергей <sh.s.e.57@gmail.com>
2024-11-01 21:17:59 +0300

    Направляю module_11_2

--- module_11_2.py
+++ module_11_2.py
@@ -1,20 +1,24 @@
+import sys
+from inspect import isfunction, getmodule
 
-import inspect
+
 def introspection_info(obj):
-    obj_type = type(obj)
-    obj_attributes = dir(obj)
 
-    def methods():
-        callable(methods)
-        return (methods.__dir__())
-
-    module = obj.__class__.__module__
-
-
-    dictionary = {'type': obj_type, 'attributes': obj_attributes, 'methods': methods(), 'module': module}
-
-    return dictionary
-
+    attributes = []
+    methods = []
+    for attr in dir(obj):
+        if callable(getattr(obj, attr)):
+            methods.append(attr)
+        else:
+            attributes.append(attr)
+    return {
+        'type': type(obj).__name__,
+        'attributes': attributes,
+        'methods': methods,
+        'module': __name__,
+        'id': id(obj),
+        'size': sys.getsizeof(obj)
+    }
 
 number_info = introspection_info(42)
 print(number_info)
