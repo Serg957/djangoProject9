aee54c0e881f4288fb7685762fec0ec4d57faacd
Сергей <sh.s.e.57@gmail.com>
2024-12-05 20:06:43 +0300

    Направляю модуль 17.3

--- pythonProject8/app/models/user.py
+++ pythonProject8/app/models/user.py
@@ -3,19 +3,19 @@ from sqlalchemy import Column, ForeignKey, Integer,String,Boolean
 from sqlalchemy.orm import  relationship
 from app.models import *
 from sqlalchemy.schema import CreateTable
-class Task(Base):
-    __tablename__ = "tasks"
+
+class User(Base):
+    __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    content = Column(String)
-    priority = Column(Integer, default=0)
-    completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
     slug = Column(String, unique=True, index=True)
 
 
 
-    users = relationship("User", back_populates="tasks")
+    tasks = relationship("Task", back_populates="users")
 
 
